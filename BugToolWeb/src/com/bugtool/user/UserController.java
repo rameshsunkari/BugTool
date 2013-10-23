a0411e8dff33a5fde931bd3d7452812a9b6bcd30
rsunkari <rameshsunkari@gmail.com>
2013-10-23 21:14:57 +0530

    changes for service, dao, model and web layers integration

--- BugToolWeb/src/com/bugtool/user/UserController.java
+++ BugToolWeb/src/com/bugtool/user/UserController.java
@@ -3,14 +3,19 @@ package com.bugtool.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bugtool.service.UserService;
 import com.bugtool.util.BugToolConstants;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+    private UserService userService;
 
 	@RequestMapping("/userLogin")
 	public ModelAndView checkUserLogin(HttpServletRequest req, HttpServletResponse resp){
@@ -18,7 +23,9 @@ public class UserController {
 		String userName = req.getParameter(BugToolConstants.USERNAME);
 		String pass = req.getParameter(BugToolConstants.PASSWORD);
 		
-		if (userName.equals(pass)){
+		boolean success = getUserService().checkUserLogin(userName, pass);
+		
+		if (success){
 			modelView = new ModelAndView("success", "message", "Logged in successfully");
 		} else {
 			modelView = new ModelAndView("failure", "message", "Log in Failed");
@@ -36,5 +43,13 @@ public class UserController {
 	public ModelAndView loginPage(HttpServletRequest req, HttpServletResponse resp){
 		req.getSession().invalidate();
 		return new ModelAndView("logout", "message", "Logged Out Successfully");
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
 	}
 }
